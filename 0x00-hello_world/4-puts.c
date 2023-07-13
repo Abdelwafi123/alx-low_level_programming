ea953ec7137f94500a3d6b87119025a33a55358e
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-13 11:12:42 -0700

    jg

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stuido.h>
+
+/**
+ * main - entry point
+ *
+ * description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
