4f57fdf26953fdf37e3f516c2c4759feee9f7262
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-15 11:41:04 -0700

    j

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:  Write a program that prints all possible
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int N1 = 0, int N2;
+
+	while (N1 <= 99)
+	{
+		N1 = N2;
+		while (N2 <= 99)
+		{
+			if (N1 != N2)
+			{
+				putchar((N1 / 10) + 48);
+				putchar((N1 % 10) + 48);
+				putchar(' ');
+				putchar((N2 / 10) + 48);
+				putchar((N2 % 10) + 48);
+				if (N1 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			N2++;
+		}
+		N1++;
+
+	}
+	putchar('\n')
+	return (0);
+}
