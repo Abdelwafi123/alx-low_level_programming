21a46fdb993aad768e3e7c5d2a7d2f0d38998fca
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-15 05:03:41 -0700

    hj

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers of base16 in lowercase
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int ch = 48;
+
+	while (ch <= 102)
+	{
+		putchar(ch);
+		if (ch == 57)
+			ch += 57
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
