a6fe3e932e91920ce24df594011d35bd3bcb032a
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-19 10:35:56 -0700

    kh

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -44,7 +44,7 @@ int main(void)
 			printf("%lu", fo);
 		initial0s = numLength(mx) - 1 - numLength(f1);
 
-		while (fo > 0 && initial > 0)
+		while (fo > 0 && initial0s > 0)
 		{
 			printf("%d", 0);
 			initial0s--;
