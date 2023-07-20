10a18fb94838a86f2f4d8f1a22f46b99a1fd4007
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-20 05:20:19 -0700

    jh

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,6 @@
  * print_most_numbers - print 0 - 9 apart
  * from 2 and 4 and  you can
  * only use _putchar twice
- *
  * Return: Always 0 (Success)
 */
 
@@ -12,7 +11,7 @@ void print_most_numbers(void)
 {
 	int num = 0;
 
-	while (num <= 0)
+	while (num <= 9)
 	{
 		if (num != 2 || num != 4)
 			_putchar(num + 48);
