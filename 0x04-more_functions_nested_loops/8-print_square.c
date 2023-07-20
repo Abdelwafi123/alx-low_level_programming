460aa5ad4e890483ee6446721d6680fd4a749e97
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-20 10:52:28 -0700

    b

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,15 +12,10 @@ void print_square(int size)
 {
 	int a, b;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
-	{
 		for (a = 1; a <= size; a++)
 		{
 			for (b = 1; b <= size; b++)
 				_putchar('#');
 			_putchar('\n');
 		}
-	}
 }
