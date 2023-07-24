6fecd1c5c49065a7f46f84c4c84d28c008efa83f
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-24 15:19:07 -0700

    h

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,8 +2,8 @@
 
 /**
  * *_strcpy - copies the string pointed to by src
- * 8dest: chat type string
- * 8src: char type string
+ * @dest: chat type string
+ * @src: char type string
  * Description: copy the string pointed to by pointer 'src'
  * to the buffer pointed to by 'dest'
  * Return: Pointer to 'dest'
