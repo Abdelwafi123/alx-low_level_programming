2a4bedf8fe90f6ba132da3da65fcd347bd36c8f6
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-26 08:22:05 -0700

    jg

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: destination string
  * @src: source string
- * Return: dest,
+ * Return: dest;
 */
 
 char *_strcat(char *dest, char *src)
@@ -14,11 +14,18 @@ char *_strcat(char *dest, char *src)
 	int p = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
+	{
 		count++;
+	}
+
 	for (i = 0; src[i] = '\0'; i++)
+	{
 		p++;
+	}
 
 	for (i = 0; i <= p; i++)
+	{
 		dest[count + i] = src[i];
+	}
 	return (dest);
 }
