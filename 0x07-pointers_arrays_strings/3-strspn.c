bde3cb53e5d11d4441080cc79a4c2980d26ec2ae
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-31 11:18:20 -0700

    jjj

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,13 +4,14 @@
  * _strspn - get the length of a prefix substring
  * @s: string to search from
  * @accept: string to search
+ * Return: unsigned int
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int itr, jtr;
 
-	for (itr = 0; s[] != '\0'; itr++)
+	for (itr = 0; s[itr] != '\0'; itr++)
 	{
 		for (jtr = 0; accept[jtr] != s[itr]; jtr++)
 		{
