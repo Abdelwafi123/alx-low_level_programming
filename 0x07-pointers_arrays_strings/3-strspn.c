96aff518e10fc15d97cde7ca2a3b100c6122c9a9
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-31 11:14:58 -0700

    bj

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix substring
+ * @s: string to search from
+ * @accept: string to search 
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int itr, jtr;
+
+	for (itr = 0; s[] != '\0'; itr++)
+	{
+		for (jtr = 0; accept[jtr] != s[itr]; jtr++)
+		{
+			if (accept[jtr] == '\0')
+				return (itr);
+		}
+	}
+	return (itr);
