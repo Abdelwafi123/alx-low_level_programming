6f3d40d130b468936603d68a190c271e8f05e46d
Abdelwafi123 <abdeloifimbarki@gmail.com>
2023-07-31 10:09:03 -0700

    hg

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * *_strchr - locates character in a string
+ * *_strchr - files memory with a constant byte.
  * @s: string to search
  * @c: character to search in s
- * Return: s or Null
+ * Return: pointer to s
 */
 
 char *_strchr(char *s, char c)
 {
 	int itr;
 
-	for (itr = 0; s[itr] >= '\0'; itr++)
+	for (itr = 0; s[itr] < '\0'; itr++)
 	{
 		if (s[itr] == s)
+		{
 			return (s + itr);
+		}
 	}
 
 	return ('\0');
